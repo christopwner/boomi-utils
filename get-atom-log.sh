d86a4630a6a4f6198d194289f338cd0539be7985
Christopher Towner <christopher.allen.towner@gmail.com>
2017-06-08 12:49:42 -0500

    lowercased vars

--- get-atom-log.sh
+++ get-atom-log.sh
@@ -21,37 +21,37 @@ usage() { echo "Usage: $0 -b <boomi_account> -a <atom_id> -u <user>:<pass> [-d <
 while getopts b:u:a:d: option; do
      case "${option}"
          in
-         b) ACCT=${OPTARG};;
-         a) ATOM=${OPTARG};;
-         u) USER=${OPTARG};;
-         d) DATE=${OPTARG};;
+         b) acct=${OPTARG};;
+         a) atom=${OPTARG};;
+         u) user=${OPTARG};;
+         d) date=${OPTARG};;
      esac
 done
 
 #defaults today's date if none provided
-if [ -z "${DATE}" ]; then
-    DATE=$(date +%Y-%m-%d)
+if [ -z "${date}" ]; then
+    date=$(date +%Y-%m-%d)
 fi
 
 #check that account, atom and user passed in
-if [ -z "${ACCT}" ] || [ -z "${ATOM}" ] || [ -z "${USER}" ]; then
+if [ -z "${acct}" ] || [ -z "${atom}" ] || [ -z "${user}" ]; then
     usage
 fi
 
 #setup request
-API_URL='https://api.boomi.com/api/rest/v1/'${ACCT}'/AtomLog'
-REQUEST='<AtomLog xmlns="http://api.platform.boomi.com/" atomId='\"$ATOM\"' logDate='\"$DATE\"' includeBin="true"/>'
+api_url='https://api.boomi.com/api/rest/v1/'${acct}'/AtomLog'
+request='<AtomLog xmlns="http://api.platform.boomi.com/" atomId='\"$atom\"' logDate='\"$date\"' includeBin="true"/>'
 
 #request download and parse url
-LOG_URL=$(curl -s -u "${USER}" -d "${REQUEST}" "${API_URL}" | xmllint --xpath 'string(/*/@url)' -)
+log_url=$(curl -s -u "${user}" -d "${request}" "${api_url}" | xmllint --xpath 'string(/*/@url)' -)
 
 #create directory for atom logs if doesn't exist
-mkdir -p ${ATOM}
+mkdir -p ${atom}
 
 #download file, retry until available
-ZIP="${ATOM}/${DATE}.zip"
-SIZE=0
-until [ $SIZE -gt 0 ]; do
-    curl -s -o ${ZIP} -u "${USER}" "${LOG_URL}"
-    SIZE=$(wc -c < $ZIP)
+zip="${atom}/${date}.zip"
+size=0
+until [ $size -gt 0 ]; do
+    curl -s -o ${zip} -u "${user}" "${log_url}"
+    size=$(wc -c < $zip)
 done
